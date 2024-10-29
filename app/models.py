822d5359c29d9adee143e62a6a7113cf1f61e5e8
Umut Topalak <umuttopalak@hotmail.com>
2024-10-29 21:05:16 +0300

    fix env variable in alembic.ini & is_used, used_at fields added

--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 
 from flask_sqlalchemy import SQLAlchemy
 
@@ -19,3 +20,14 @@ class TwoFactorAuthModel(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     code = db.Column(db.String(6), nullable=False)
     expires_at = db.Column(db.DateTime, nullable=False)
+    used_at = db.Column(db.DateTime, nullable=True)
+    is_used = db.Column(db.Boolean, nullable=False, default=False)
+    
+    def is_valid(self):
+        return self.used_at is None and self.expires_at > datetime.utcnow()
+    
+    def mark_as_used(self):
+        self.used_at = datetime.utcnow()
+        self.is_used = True
+        db.session.commit()
+
